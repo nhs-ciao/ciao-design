79580abf6f49956672a4eba0d86608267c0f514b
Adam Hatherly <adam.hatherly@hscic.gov.uk>
2015-06-12 21:27:03 +0100

    Began populating business analysis artefacts

---
+++ CIPCatalogue/TransferOfCare/ImprovePatientCare.mm
@@ -0,0 +1,15 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1434139066199" ID="ID_1361208644" MODIFIED="1434139654547" STYLE="fork" TEXT="Goal: Improve Patient Care">
+<node CREATED="1434139657810" ID="ID_1404559995" MODIFIED="1434139682034" POSITION="right" TEXT="Stakeholder: Secondary Care Clinician">
+<node CREATED="1434139689357" ID="ID_1861523975" MODIFIED="1434139727314" TEXT="Capability: Take information from my eDischarge system and send it to the GP electronically"/>
+<node CREATED="1434140685128" ID="ID_1793502773" MODIFIED="1434140771700" TEXT="Capability: Identify failures so a summary can be sent manually"/>
+</node>
+<node CREATED="1434139192630" HGAP="155" ID="ID_926418086" MODIFIED="1434139742746" POSITION="right" TEXT="Stakeholder: GP" VSHIFT="36">
+<node CREATED="1434139211367" ID="ID_913235504" MODIFIED="1434139651019" TEXT="Capability: Timely information about secondary care episodes"/>
+<node CREATED="1434139257554" ID="ID_13121608" MODIFIED="1434139644526" TEXT="Capability: Up-to-date information about medications"/>
+<node CREATED="1434139319749" ID="ID_508492110" MODIFIED="1434139639821" TEXT="Capability: Comprehensive record of patient care"/>
+<node CREATED="1434139367538" ID="ID_1417280401" MODIFIED="1434139634958" TEXT="Capability: Consistent record of episodes"/>
+</node>
+</node>
+</map>
